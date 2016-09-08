66d86bfb57d1967ad234b7f049b72a3690cf60b5
Ken Snyder <ken@ken.net>
2016-09-08 16:55:49 +0100

    chat item component

--- app/models/message.js
+++ app/models/message.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
-import Ember from 'ember';
-const { attr, belongsTo } = Ember;
+const { attr, belongsTo } = DS;
 
 export default DS.Model.extend({
   user: belongsTo('user'),
